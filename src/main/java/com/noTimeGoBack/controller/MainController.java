797639aa0f043da63e193804855272858149f506
handongqingxue <1417140290@qq.com>
2023-12-03 23:19:43 +0800

    1

--- src/main/java/com/noTimeGoBack/controller/MainController.java
+++ src/main/java/com/noTimeGoBack/controller/MainController.java
@@ -34,6 +34,7 @@ public class MainController {
 			titleList.add(Part1Util.CHAP6_TIT_NAME);
 			titleList.add(Part1Util.CHAP7_TIT_NAME);
 			titleList.add(Part1Util.CHAP8_TIT_NAME);
+			titleList.add(Part1Util.CHAP9_TIT_NAME);
 			
 			request.setAttribute("titleList", titleList);
 			
